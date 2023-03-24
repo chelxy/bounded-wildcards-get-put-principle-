8e58a1c53ed375c8403a149ca7ecb3810481d473
Michel Morais <valentimmichel@gmail.com>
2023-03-24 12:00:32 -0300

    Princípio get/put - covariância

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+//Princípio get/put - covariância
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		
+		List<? extends Number> list = intList;
+		
+		Number x = list.get(0);// Posso acessar, mas não posso adicionar
+		
+		//list.add(20); // erro de compilacao
+	}
+
+}
